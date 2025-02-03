e69c6ce1973c91ef5c4c2d1b6fe0d0f73660f803
tonmoy-1030 <tonmoy.hrd.tk@gmail.com>
2025-02-03 09:54:50 +0600

    Add custom user admin with formatted last login

--- jobs/admin.py
+++ jobs/admin.py
@@ -14,3 +14,34 @@ class JobAdmin(admin.ModelAdmin):
 
     
 admin.site.register(Job, JobAdmin)
+
+
+# app/admin.py (or any admin.py in your project)
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+
+class CustomUserAdmin(UserAdmin):
+    # Define the columns to display in the user list
+    list_display = (
+        'username', 
+        'email', 
+        'first_name', 
+        'last_name', 
+        'is_staff', 
+        'formatted_last_login',  # Custom formatted last login
+        'date_joined'            # Optional: Add other fields
+    )
+
+    # Method to format last_login
+    def formatted_last_login(self, obj):
+        if obj.last_login:
+            return obj.last_login.strftime("%Y-%m-%d %H:%M:%S")
+        return "Never logged in"
+    formatted_last_login.short_description = 'Last Login'  # Column header
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
