2f7ddfcbf3da2dba79da1f03112e53e0396d2fd6
tonmoy-1030 <tonmoy.hrd.tk@gmail.com>
2025-05-04 16:53:03 +0600

    Refactor admin registration for employee-related models to use ModelAdmin classes for better organization and functionality

--- employees/admin.py
+++ employees/admin.py
@@ -3,12 +3,46 @@ from .models import (Employee, EmployeeDetails, SeperationStatus,
                      EmployeeConfirmation, TransferOrder, PostingOrder, SalaryInfo)
 
 # Register your models here.
-admin.site.register(Employee)
-admin.site.register(EmployeeDetails)
-admin.site.register(SeperationStatus)
-admin.site.register(EmployeeConfirmation)
-admin.site.register(TransferOrder)
-admin.site.register(PostingOrder)
-admin.site.register(SalaryInfo)
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('EID', 'name', 'designation', 'department', 'unit', 'email', 'mobile_no')
+    search_fields = ('EID', 'name')
+    list_filter = ('department', 'unit')
+    ordering = ('EID',)
 
+@admin.register(EmployeeDetails)
+class EmployeeDetailsAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'date_of_birth', 'marital_status', 'blood_group')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('employee__EID',)
+
+@admin.register(SeperationStatus)
+class SeperationStatusAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'resign_date', 'reason')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('resign_date',)
+
+@admin.register(EmployeeConfirmation)
+class EmployeeConfirmationAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'effective_date', 'remarks')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('effective_date',)
+
+@admin.register(TransferOrder)
+class TransferOrderAdmin(admin.ModelAdmin):
+    list_display = ('employee',  'effective_date')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('effective_date',)
+
+@admin.register(PostingOrder)
+class PostingOrderAdmin(admin.ModelAdmin):
+    list_display = ('employee',  'effective_date')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('effective_date',)
+
+@admin.register(SalaryInfo)
+class SalaryInfoAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'salary')
+    search_fields = ('employee__name', 'employee__EID')
+    ordering = ('-employee__DOJ',)
 
